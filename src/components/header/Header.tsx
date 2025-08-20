3380889a5f67ac12990f9349b27f619c3c13e8ed
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-20 10:54:26 +0200

    🎉(tada): fixed scss and basic normalization

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Pumpkin Shop</h1>
+            </header>
+        </>
+    )
+}
