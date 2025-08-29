b5e7a58384713f39e41c0eeefb22aabaff23f863
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-29 14:27:55 +0200

    💬(lang): fixed misspelled word

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,7 +6,7 @@ export const About = () => {
         <>
             <section>
                 <div className='text-wrap'>
-                    <h2>Welcome to <span>Pumkin Brew!</span></h2>
+                    <h2>Welcome to <span>Pumpkin Brew!</span></h2>
                     <p>At Pumpkin Brew we create handcrafted syrups designed to turn your coffee into the perfect seasonal drink. From creamy pumpkin to warm cinnamon and rich caramel, our flavors are made to bring the cozy feeling of fall to every sip. Whether you want to elevate your morning latte or create a café experience at home, we have the syrup that makes your cup unforgettable.</p>
                     <button>Scroll to shop <span className="material-symbols-outlined arrow">
                         arrow_downward
