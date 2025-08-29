b4e3a9a14bdf22ddba1ab9b16ed8ad0dccf120cb
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-08-29 22:04:06 +0200

    💄(style): fixed style and responsivness

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router';
 import './App.scss'
 import { router } from './Router'
 
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,7 +6,7 @@ export const About = () => {
     <>
       <section>
         <div className="text-wrap">
-          <h2>
+          <h2 className='welcome-text'>
             Welcome to <span>Pumpkin Brew!</span>
           </h2>
           <p>
@@ -17,7 +17,7 @@ export const About = () => {
             morning latte or create a café experience at home, we have the syrup
             that makes your cup unforgettable.
           </p>
-          <button>
+          <button className='scroll-btn'>
             Scroll to shop{' '}
             <span className="material-symbols-outlined arrow">
               arrow_downward
@@ -25,20 +25,20 @@ export const About = () => {
           </button>
         </div>
         <img className="coffe-img" src={mug} alt="coffe mug with a splash" />
+        <div className="custom-shape-divider-bottom-1756470733">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
       </section>
-      <div className="custom-shape-divider-bottom-1756470733">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div>
     </>
   );
 };
