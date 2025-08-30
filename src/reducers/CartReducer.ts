3ac33003b3c1b594b8496c24cb98ebe205a52a09
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-08-30 20:16:33 +0200

    ✨(feature): add reducer for cart and products

---
+++ src/reducers/CartReducer.ts
@@ -0,0 +1,66 @@
+import type { CartItem } from "../models/CartItem";
+import type { Product } from "../models/Product";
+
+export enum CartActionTypes {
+    ADDED,
+    REMOVED,
+    INCREASED,
+    DECREASED,
+};
+
+export type CartAction = {
+    type: CartActionTypes;
+    payload: string;
+};
+
+export const CartReducer = (cart: CartItem[], action: CartAction) => {
+    switch (action.type) {
+        case CartActionTypes.ADDED: {
+            const productToAdd: Product = JSON.parse(action.payload);
+
+            const foundCartItem = cart.find(
+                (ci) => ci.product.id === productToAdd.id,
+            );
+
+            if (foundCartItem) {
+                return cart.map((ci) => {
+                    if (ci.product.id === foundCartItem.product.id) {
+                        return { ...ci, amount: ci.amount + 1 };
+                    }
+                    return ci;
+                });
+            }
+
+            const CartActionTypes.REMOVED: {
+                const returnValue = cart.filter(
+                (ci) => ci.product.id !== +action.payload,
+            );
+            return returnValue;
+        }
+
+        case CartActionTypes.INCREASED: {
+            const productId = +action.payload;
+
+            const returnValue = cart.map((ci) => {
+                if (ci.product.id === productId) {
+                    return { ...ci, amount: ci.amount + 1 };
+                }
+                return ci;
+            });
+            return returnValue;
+        }
+
+        case CartActionTypes.DECREASED: {
+            const productId = +action.payload;
+
+            const returnValue = cart.map((ci) => {
+                if (ci.product.id === productId) {
+                    return { ...ci, amount: ci.amount - 1 };
+                }
+                return ci;
+            });
+            return returnValue;
+        }
+        default: return cart;
+    }
+};
