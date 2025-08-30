e1936ec9fa0e7bbb01567f9a406afd5d7ea94499
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-08-30 20:37:44 +0200

    🚧(in progress): put things together with context, cart and layout

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,12 +1,15 @@
 // Steg 2 efter models, gör state (hook eller reducer t.ex) tillgängligt för alla komponenter
 
-import { createContext } from "react";
+import { createContext, type Dispatch } from "react";
 import type { CartItem } from "../models/CartItem"
+import type { CartAction } from "../reducers/CartReducer";
 
 type CartContextType = {
     cart: CartItem[];
+    dispatch: Dispatch<CartAction>;
 };
 
 export const CartContext = createContext<CartContextType>({
     cart: [],
-})
+    dispatch: () => {},
+});
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,17 +1,25 @@
 import { Outlet } from 'react-router';
 import { Header } from '../components/header/Header';
 import { Footer } from '../components/footer/Footer';
+import { useReducer } from 'react';
+import { CartReducer } from '../reducers/CartReducer';
+import { CartContext } from '../contexts/CartContext';
 
 // Hur sidan ska formas, i detta fallet - header och footer på alla sidor och outlet (innehållet) mellan.
 
 export const Layout = () => {
+  const [cart, dispatch] = useReducer(
+    CartReducer,
+    JSON.parse(localStorage.getItem("cart") || "[]"),
+  );
+
   return (
-    <>
+    <CartContext.Provider value={{cart, dispatch}}>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer/>
-    </>
+    </CartContext.Provider>
   );
 };
