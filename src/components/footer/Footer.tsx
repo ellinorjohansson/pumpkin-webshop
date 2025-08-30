f5bdd9452deaa1754bc3db8d6d648c360762522f
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-08-30 19:33:27 +0200

    ✨(feature): add scroll to top btn in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,21 @@
 import './footer.scss';
 
 export const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <>
             <section className='f-wrapper'>
                 <h2 className='logo'>Pumpkin Brew</h2>
+                <button
+                    className="scroll-to-top"
+                    onClick={scrollToTop}
+                    aria-label="Scroll to top"
+                >
+                    <span className="material-symbols-outlined">arrow_upward</span>
+                </button>
             </section>
         </>
     )
