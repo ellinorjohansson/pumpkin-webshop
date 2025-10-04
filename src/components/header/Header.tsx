cd9b710c0e7887108eb913f3f2d9783db5880712
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-10-04 19:43:44 +0200

    ✨(feature): added cart button and fixed style on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,9 @@ export const Header = () => {
         <>
             <header>
                 <h1>Pumpkin Brew</h1>
+                <button className='cart-btn'><span className="material-symbols-outlined cart-icon">
+                    shopping_cart
+                </span></button>
             </header>
         </>
     )
