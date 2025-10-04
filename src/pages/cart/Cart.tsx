f0cdd3c3157c2c4ba241608c5dce55542c7c5868
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-10-04 21:09:40 +0200

    ✨(feature): added text if cart is empty and formatting to code

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,48 +1,94 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import { CartActionTypes } from "../../reducers/CartReducer";
+import './cart.scss';
 
 export const Cart = () => {
   const { cart, dispatch } = useContext(CartContext);
 
   return (
-    <ul>
-      {cart.map((ci) => (
-        <li key={ci.product.name}>
-          <button
-            onClick={() =>
-              dispatch({
-                type: CartActionTypes.DECREASED,
-                payload: ci.product.id.toString(),
-              })
-            }
-            disabled={ci.amount === 1}
-          >
-            -
-          </button>
-          {ci.product.name} - {ci.amount}
-          <button
-            onClick={() =>
-              dispatch({
-                type: CartActionTypes.INCREASED,
-                payload: ci.product.id.toString(),
-              })
-            }
-          >
-            +
-          </button>
-          <button
-            onClick={() =>
-              dispatch({
-                type: CartActionTypes.REMOVED,
-                payload: ci.product.id.toString(),
-              })
-            }
-          >
-            Ta bort
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <section className="cart-wrapper">
+        {cart.length === 0 ? (
+          <p className="empty-cart">Cart empty</p>
+        ) : (
+          <ul>
+            {cart.map((ci) => (
+              <li key={ci.product.name}>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: CartActionTypes.DECREASED,
+                      payload: ci.product.id.toString(),
+                    })
+                  }
+                  disabled={ci.amount === 1}
+                >
+                  -
+                </button>
+                <span className="product-name">
+                  {ci.product.name} - {ci.amount}
+                </span>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: CartActionTypes.INCREASED,
+                      payload: ci.product.id.toString(),
+                    })
+                  }
+                >
+                  +
+                </button>
+                <button
+                  className="remove"
+                  onClick={() =>
+                    dispatch({
+                      type: CartActionTypes.REMOVED,
+                      payload: ci.product.id.toString(),
+                    })
+                  }
+                >
+                  Ta bort
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+
+      <section className="form-wrapper">
+        <form>
+          <label>
+            <h2>Firstname</h2>
+            <input type="text" />
+          </label>
+          <label>
+            <h2>Lastname</h2>
+            <input type="text" />
+          </label>
+          <label>
+            <h2>Address</h2>
+            <input type="text" />
+          </label>
+          <label>
+            <h2>Email</h2>
+            <input type="text" />
+          </label>
+          <label>
+            <h2>Address</h2>
+            <input type="text" />
+          </label>
+          <label className="checkbox-label">
+            <h2>Terms and condition</h2>
+            <input type="checkbox" />
+          </label>
+          <label className="checkbox-label">
+            <h2>Newsletter</h2>
+            <input type="checkbox" />
+          </label>
+          <input type="submit" value="Send" />
+        </form>
+      </section>
+    </>
   );
 };
