0cdb77d4323819b211a057d71600a6c02869e451
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-10-04 19:50:00 +0200

    ✨(feature): added link functionality to cart button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router';
 import './Header.scss';
 
 export const Header = () => {
+    const navigate = useNavigate();
     return (
         <>
             <header>
                 <h1>Pumpkin Brew</h1>
-                <button className='cart-btn'><span className="material-symbols-outlined cart-icon">
+                <button onClick={() => navigate("/cart")} className='cart-btn'><span className="material-symbols-outlined cart-icon">
                     shopping_cart
                 </span></button>
             </header>
